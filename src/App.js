ea9e75c57200fa1f1bd930f980b3f34ec7c80894
Shelby Speegle <shelbyspeegle@gmail.com>
2017-03-04 13:36:02 -0700

    initial commit.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const style = {};
+
+export default class App extends Component {
+    render() {
+        return (
+            <div style = { style }>
+                Yay
+            </div>
+        );
+    }
+};
