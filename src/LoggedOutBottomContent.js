18366a5190f673a1d681b8b4ddabccd1cb285a6a
Shelby Speegle <shelbyspeegle@gmail.com>
2017-04-01 16:20:51 -0600

    login looks a little nicer

--- src/LoggedOutBottomContent.js
+++ src/LoggedOutBottomContent.js
@@ -1,18 +1,54 @@
 import React, { Component, PropTypes } from 'react';
+import TextField from 'material-ui/TextField';
+import Paper from 'material-ui/Paper';
+import RaisedButton from 'material-ui/RaisedButton';
+
+const style = {
+    textAlign: 'center',
+};
+
+const paperStyle = {
+    width: '90%',
+    margin: '20px auto',
+    padding: '15px',
+};
+
+const buttonStyle = {
+    marginTop: '10px',
+};
+
+const inputWrapStyle = {
+    display: 'inline-block',
+    textAlign: 'right',
+};
+
+const loginStyle = {
+    fontSize: '18px',
+    fontWeight: 'bold',
+    marginTop: '15px',
+    marginBottom: '0',
+    padding: '0',
+};
 
 export default class LoggedOutBottomContent extends Component {
     render() {
         return (
-            <div>
-                log in here!
-                <br />
-                <input />
-                <br />
-                <input type = 'password' />
-                <br />
-                <button type = 'submit' onClick = { this.props.handleLogin }>
-                    log in!
-                </button>
+            <div style = { style }>
+                <Paper style = { paperStyle } zDepth = { 2 }>
+                    <p style = { loginStyle }>Login</p>
+                    <div style = { inputWrapStyle }>
+                        <TextField floatingLabelText = 'Username' />
+                        <br />
+                        <TextField floatingLabelText = 'Password' type = 'password' />
+                    </div>
+                    <br />
+                    <RaisedButton
+                        style = { buttonStyle }
+                        label = 'Log In!'
+                        type = 'submit'
+                        onClick = { this.props.handleLogin }
+                    />
+                </Paper>
             </div>
         );
     }
