ef062c5320348f2d3616341ece718a2671642ee5
LinkofOrigin <jesselink333@gmail.com>
2017-04-03 17:10:27 -0600

    Working on valid/invalid input for logging in. Should simulate incorrect logins, use fake login credentials for successful

--- src/LoggedOutBottomContent.js
+++ src/LoggedOutBottomContent.js
@@ -9,17 +9,13 @@ const style = {
 
 const paperStyle = {
     width: '90%',
+    maxWidth: '700px',
     margin: '20px auto',
     padding: '15px',
 };
 
 const buttonStyle = {
     marginTop: '10px',
-};
-
-const inputWrapStyle = {
-    display: 'inline-block',
-    textAlign: 'right',
 };
 
 const loginStyle = {
@@ -30,24 +26,65 @@ const loginStyle = {
     padding: '0',
 };
 
+const inputWrapStyle = {
+    display: 'inline-block',
+    textAlign: 'right',
+};
+
+const initialState = {
+    usernameValue: "",
+    passwordValue: "",
+};
+
 export default class LoggedOutBottomContent extends Component {
+    constructor() {
+        super();
+        this.state = initialState;
+    }
+    
+    handleLoginClick = () => {
+        let userVal = this.state.usernameValue;
+        console.log(userVal);
+        let passVal = this.state.passwordValue;
+        console.log(passVal);
+    };
+    
+    handleUsernameChange = (e) => {
+        console.log(e.target.value);
+        this.setState({ usernameValue: e.target.value })
+    };
+    
+    handlePasswordChange = (e) => {
+        this.setState({ passwordValue: e.target.value })
+    };
+    
     render() {
         return (
             <div style = { style }>
                 <Paper style = { paperStyle } zDepth = { 2 }>
-                    <p style = { loginStyle }>Login</p>
+                    <p style = { loginStyle }>Just for KIX</p>
                     <div style = { inputWrapStyle }>
-                        <TextField floatingLabelText = 'Username' />
+                        <TextField
+                            floatingLabelText = 'Username'
+                            value = { this.state.usernameValue }
+                            onChange = { this.handleUsernameChange }
+                        />
                         <br />
-                        <TextField floatingLabelText = 'Password' type = 'password' />
+                        <TextField
+                            floatingLabelText = 'Password'
+                            type = 'password'
+                            value = { this.state.passwordValue }
+                            onChange = { this.handlePasswordChange }
+                        />
                     </div>
                     <br />
                     <RaisedButton
                         style = { buttonStyle }
                         label = 'Log In!'
                         type = 'submit'
-                        onClick = { this.props.handleLogin }
+                        onClick = { this.props.handleLoginClick }
                     />
+                    
                 </Paper>
             </div>
         );
